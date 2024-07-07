03e91ed00525a663e6f66f18ff6fca3b25ee2b5b
alyumi <lanton.1999@mail.ru>
2024-07-07 23:53:44 +0500

    add testReported add parametrized tests

--- src/test/java/org/example/mathTest.java
+++ src/test/java/org/example/mathTest.java
@@ -1,15 +1,31 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.example.math.Operations.multiply;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class mathTest {
 
-    @Test
-    public void multiplyTest(){
-        assertEquals(50, multiply(10, 5));
+    @ParameterizedTest
+    @MethodSource(value = "multiplyTestArguments")
+    public void multiplyTest(Integer expected, Integer a, Integer b){
+        assertEquals(expected, multiply(a, b));
     }
 
+
+    public static Stream<Arguments> multiplyTestArguments() {
+        return Stream.of(
+                Arguments.of(50, 10, 5),
+                Arguments.of(5, 1, 5),
+                Arguments.of(-50, -10, 5),
+                Arguments.of(0, 0, 5),
+                Arguments.of(0, 10, 0)
+        );
+    }
 }
